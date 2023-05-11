fd5cd99ae293885284aeaac265ad882c71600f59
FernandoVN98 <38290736+FernandoVN98@users.noreply.github.com>
2023-05-11 16:33:53 +0200

    Create launch_regression.sh

---
+++ tests/Nested/launch_regression.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./deploy_app.sh &
+
+agent_PID=${!}
+
+sleep 5
+
+./launch_app.sh 127.0.0.1 46101 main_regression.py
+
+echo "Waiting for result"
+wait ${agent_PID}
+
+grep "Training time" "${agent_log_dir}/jobs/job1_NEW.out"
